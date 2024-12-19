870cc6bd2b12eaa84f3b6a599cfbe5a1823e1ed3
Ashish <ashishsaud498@gmail.com>
2024-12-20 01:22:05 +0545

    advanced todo app is completed

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-let tasks = [{ text: 'Task 1', completed: false }, { text: 'Task 2', completed: true }];
+document.addEventListener('DOMContentLoaded', () => {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+  if (storedTasks) {
+    storedTasks.forEach(task => tasks.push(task));
+    updateTaskList();
+    updateStats();
+  }
+});
+
+let tasks = [];
+
+const saveTasks = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 updateTaskList()
-
 
 document.getElementById('newTask')
   .addEventListener("click", (e) => {
     e.preventDefault();
-    // console.log('button clicked');
     addTask();
   });
 
@@ -17,9 +29,10 @@ const addTask = () => {
 
   if (text) {
     tasks.push({ text: text, completed: false });
-    // console.log(tasks);
 
     updateTaskList()
+    updateStats();
+    saveTasks();
   }
 }
 
@@ -28,8 +41,6 @@ function updateTaskList() {
   taskList.innerHTML = '';
 
   tasks.forEach((task, index) => {
-    // console.log(task);
-
     const listItem = document.createElement('li');
 
     listItem.innerHTML = `
@@ -45,8 +56,8 @@ function updateTaskList() {
       </div>
     </div>
     `;
-
-    listItem.addEventListener('change', () => toggleTaskComplete(index))
+    listItem.querySelector('.checkbox').addEventListener('change', () => toggleTaskComplete(index));
+    // listItem.addEventListener('change', () => toggleTaskComplete(index))
     taskList.appendChild(listItem);
   });
 
@@ -55,18 +66,22 @@ function updateTaskList() {
 function toggleTaskComplete(index) {
   tasks[index].completed = !tasks[index].completed;
   updateTaskList();
+  updateStats();
+  saveTasks();
 }
 
 
 const deleteTask = (index) => {
   tasks.splice(index, 1);
   updateTaskList();
+  updateStats();
+  saveTasks();
 }
 
 
 const editTask = (index) => {
-  const taskItem= document.querySelectorAll('.taskItem')
-  
+  const taskItem = document.querySelectorAll('.taskItem')
+
   taskItem.forEach((taskItem, i) => {
     if (i === index) {
       const taskText = taskItem.querySelector('p');
@@ -91,4 +106,67 @@ const editTask = (index) => {
       });
     }
   });
+
+  updateStats();
+  saveTasks();
 };
+
+const updateStats = () => {
+  const completeTasks = tasks.filter(task => task.completed).length;
+  const totalTasks = tasks.length;
+  const progress = (completeTasks / totalTasks) * 100;
+  const progressBar = document.getElementById('progress');
+
+  progressBar.style.width = `${progress}%`;
+
+  document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`;
+
+  if (tasks.length && completeTasks === totalTasks) {
+    blastConfettiAnimation();
+  }
+
+}
+
+//external library for confetti animation 
+
+const blastConfettiAnimation = () => {
+  const count = 200,
+    defaults = {
+      origin: { y: 0.7 },
+    };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+
+  fire(0.2, {
+    spread: 60,
+  });
+
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
